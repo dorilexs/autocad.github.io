e02ae5955306755e219251b6ebfbbbf04b45fbb4
dorilex <108504269+dorilex@users.noreply.github.com>
2022-08-22 21:24:53 -0500

    tres iconos

--- js/script1.js
+++ js/script1.js
@@ -6,6 +6,14 @@
     const messenger = document.querySelector('.C-021-segundo');
     const chat = document.querySelector('.C-021-tercero');
 
+
+
+    const formessenger = document.querySelector('.C-024none-messenger');
+    const forchat = document.querySelector('.C-024none-chat');
+
+    const cerrarmessenger = document.querySelector('.C-030-1close-messenger');
+    const cerrarchat = document.querySelector('.C-030-1close-chat')
+
     magia.addEventListener('click', () =>{
         aparecer.classList.remove('C-024none');
         magia.classList.add('C-021none');
@@ -18,4 +26,33 @@
         messenger.classList.remove('C-021none');
         chat.classList.remove('C-021none');
     });
+
+
+    messenger.addEventListener('click', () =>{
+        formessenger.classList.remove('C-024none');
+        magia.classList.add('C-021none');
+        messenger.classList.add('C-021none');
+        chat.classList.add('C-021none');
+    });
+    cerrarmessenger.addEventListener('click', () =>{
+        formessenger.classList.add('C-024none');
+        magia.classList.remove('C-021none');
+        messenger.classList.remove('C-021none');
+        chat.classList.remove('C-021none');
+    });
+
+
+    chat.addEventListener('click', () =>{
+        forchat.classList.remove('C-024none');
+        magia.classList.add('C-021none');
+        messenger.classList.add('C-021none');
+        chat.classList.add('C-021none');
+    });
+    cerrarchat.addEventListener('click', () =>{
+        forchat.classList.add('C-024none');
+        magia.classList.remove('C-021none');
+        messenger.classList.remove('C-021none');
+        chat.classList.remove('C-021none');
+    });
+
 }
