a9532420f5b5be2223b502dff30bdd96eb1aee57
dorilex <108504269+dorilex@users.noreply.github.com>
2022-08-22 20:57:10 -0500

    mas iconos

--- js/script1.js
+++ js/script1.js
@@ -1,15 +1,21 @@
 {
     const aparecer = document.querySelector('.C-024');
-    const desaparecer = document.querySelector('.C-019none');
     const cerrar = document.querySelector('.C-030-1close');
     const magia = document.querySelector('.C-021');
+
+    const messenger = document.querySelector('.C-021-segundo');
+    const chat = document.querySelector('.C-021-tercero');
 
     magia.addEventListener('click', () =>{
         aparecer.classList.remove('C-024none');
         magia.classList.add('C-021none');
+        messenger.classList.add('C-021none');
+        chat.classList.add('C-021none');
     });
     cerrar.addEventListener('click', () =>{
         aparecer.classList.add('C-024none');
         magia.classList.remove('C-021none');
+        messenger.classList.remove('C-021none');
+        chat.classList.remove('C-021none');
     });
 }
