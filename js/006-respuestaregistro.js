7c955335628ce141bb513a81e21f9aee068fc4a5
dorilex <108504269+dorilex@users.noreply.github.com>
2022-09-12 17:23:12 -0500

    50descuentoTerminado

--- js/006-respuestaregistro.js
+++ js/006-respuestaregistro.js
@@ -1,14 +1,228 @@
 {
     const padre = document.querySelector('.Spadreseccion-006');
-    const padremirando = document.querySelector('.Spadreseccion-006-none');
-    const cerrar = document.querySelector('.a009-006')
-    const vida = document.querySelector('.C-042-5-11');
 
-    vida.addEventListener('click', () =>{
+    const cerrar1 = document.querySelector('.a009-006none001');
+    const cerrar2 = document.querySelector('.a009-006none002');
+    const cerrar3 = document.querySelector('.a009-006none003');
+    const cerrar4 = document.querySelector('.a009-006none004');
+    const cerrar5 = document.querySelector('.a009-006none005');
+    const cerrar6 = document.querySelector('.a009-006none006');
+    const cerrar7 = document.querySelector('.a009-006none007');
+    const cerrar8 = document.querySelector('.a009-006none008');
+    const cerrar9 = document.querySelector('.a009-006none009');
+    const cerrar10 = document.querySelector('.a009-006none010');
+    const cerrar11 = document.querySelector('.a009-006none011');
+
+    const vida1 = document.querySelector('.C-042-5-11none001');
+    const vida2 = document.querySelector('.C-042-5-11none002');
+    const vida3 = document.querySelector('.C-042-5-11none003');
+    const vida4 = document.querySelector('.C-042-5-11none004');
+    const vida5 = document.querySelector('.C-042-5-11none005');
+    const vida6 = document.querySelector('.C-042-5-11none006');
+    const vida7 = document.querySelector('.C-042-5-11none007');
+    const vida8 = document.querySelector('.C-042-5-11none008');
+    const vida9 = document.querySelector('.C-042-5-11none009');
+    const vida10 = document.querySelector('.C-042-5-11none010');
+    const vida11 = document.querySelector('.C-042-5-11none011');
+
+    const bloque1 = document.querySelector('.a001-006none001');
+    const bloque2 = document.querySelector('.a001-006none002');
+    const bloque3 = document.querySelector('.a001-006none003');
+    const bloque4 = document.querySelector('.a001-006none004');
+    const bloque5 = document.querySelector('.a001-006none005');
+    const bloque6 = document.querySelector('.a001-006none006');
+    const bloque7 = document.querySelector('.a001-006none007');
+    const bloque8 = document.querySelector('.a001-006none008');
+    const bloque9 = document.querySelector('.a001-006none009');
+    const bloque10 = document.querySelector('.a001-006none010');
+    const bloque11 = document.querySelector('.a001-006none011');
+
+    vida1.addEventListener('click', () =>{
         padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.remove('a001-006nonefull');
+        bloque2.classList.add('a001-006nonefull');
+        bloque3.classList.add('a001-006nonefull');
+        bloque4.classList.add('a001-006nonefull');
+        bloque5.classList.add('a001-006nonefull');
+        bloque6.classList.add('a001-006nonefull');
+        bloque7.classList.add('a001-006nonefull');
+        bloque8.classList.add('a001-006nonefull');
+        bloque9.classList.add('a001-006nonefull');
+        bloque10.classList.add('a001-006nonefull');
+        bloque11.classList.add('a001-006nonefull');
+    })
+    vida2.addEventListener('click', () =>{
+        padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.add('a001-006nonefull');
+        bloque2.classList.remove('a001-006nonefull');
+        bloque3.classList.add('a001-006nonefull');
+        bloque4.classList.add('a001-006nonefull');
+        bloque5.classList.add('a001-006nonefull');
+        bloque6.classList.add('a001-006nonefull');
+        bloque7.classList.add('a001-006nonefull');
+        bloque8.classList.add('a001-006nonefull');
+        bloque9.classList.add('a001-006nonefull');
+        bloque10.classList.add('a001-006nonefull');
+        bloque11.classList.add('a001-006nonefull');
+    })
+    vida3.addEventListener('click', () =>{
+        padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.add('a001-006nonefull');
+        bloque2.classList.add('a001-006nonefull');
+        bloque3.classList.remove('a001-006nonefull');
+        bloque4.classList.add('a001-006nonefull');
+        bloque5.classList.add('a001-006nonefull');
+        bloque6.classList.add('a001-006nonefull');
+        bloque7.classList.add('a001-006nonefull');
+        bloque8.classList.add('a001-006nonefull');
+        bloque9.classList.add('a001-006nonefull');
+        bloque10.classList.add('a001-006nonefull');
+        bloque11.classList.add('a001-006nonefull');
+    })
+    vida4.addEventListener('click', () =>{
+        padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.add('a001-006nonefull');
+        bloque2.classList.add('a001-006nonefull');
+        bloque3.classList.add('a001-006nonefull');
+        bloque4.classList.remove('a001-006nonefull');
+        bloque5.classList.add('a001-006nonefull');
+        bloque6.classList.add('a001-006nonefull');
+        bloque7.classList.add('a001-006nonefull');
+        bloque8.classList.add('a001-006nonefull');
+        bloque9.classList.add('a001-006nonefull');
+        bloque10.classList.add('a001-006nonefull');
+        bloque11.classList.add('a001-006nonefull');
+    })
+    vida5.addEventListener('click', () =>{
+        padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.add('a001-006nonefull');
+        bloque2.classList.add('a001-006nonefull');
+        bloque3.classList.add('a001-006nonefull');
+        bloque4.classList.add('a001-006nonefull');
+        bloque5.classList.remove('a001-006nonefull');
+        bloque6.classList.add('a001-006nonefull');
+        bloque7.classList.add('a001-006nonefull');
+        bloque8.classList.add('a001-006nonefull');
+        bloque9.classList.add('a001-006nonefull');
+        bloque10.classList.add('a001-006nonefull');
+        bloque11.classList.add('a001-006nonefull');
+    })
+    vida6.addEventListener('click', () =>{
+        padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.add('a001-006nonefull');
+        bloque2.classList.add('a001-006nonefull');
+        bloque3.classList.add('a001-006nonefull');
+        bloque4.classList.add('a001-006nonefull');
+        bloque5.classList.add('a001-006nonefull');
+        bloque6.classList.remove('a001-006nonefull');
+        bloque7.classList.add('a001-006nonefull');
+        bloque8.classList.add('a001-006nonefull');
+        bloque9.classList.add('a001-006nonefull');
+        bloque10.classList.add('a001-006nonefull');
+        bloque11.classList.add('a001-006nonefull');
+    })
+    vida7.addEventListener('click', () =>{
+        padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.add('a001-006nonefull');
+        bloque2.classList.add('a001-006nonefull');
+        bloque3.classList.add('a001-006nonefull');
+        bloque4.classList.add('a001-006nonefull');
+        bloque5.classList.add('a001-006nonefull');
+        bloque6.classList.add('a001-006nonefull');
+        bloque7.classList.remove('a001-006nonefull');
+        bloque8.classList.add('a001-006nonefull');
+        bloque9.classList.add('a001-006nonefull');
+        bloque10.classList.add('a001-006nonefull');
+        bloque11.classList.add('a001-006nonefull');
+    })
+    vida8.addEventListener('click', () =>{
+        padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.add('a001-006nonefull');
+        bloque2.classList.add('a001-006nonefull');
+        bloque3.classList.add('a001-006nonefull');
+        bloque4.classList.add('a001-006nonefull');
+        bloque5.classList.add('a001-006nonefull');
+        bloque6.classList.add('a001-006nonefull');
+        bloque7.classList.add('a001-006nonefull');
+        bloque8.classList.remove('a001-006nonefull');
+        bloque9.classList.add('a001-006nonefull');
+        bloque10.classList.add('a001-006nonefull');
+        bloque11.classList.add('a001-006nonefull');
+    })
+    vida9.addEventListener('click', () =>{
+        padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.add('a001-006nonefull');
+        bloque2.classList.add('a001-006nonefull');
+        bloque3.classList.add('a001-006nonefull');
+        bloque4.classList.add('a001-006nonefull');
+        bloque5.classList.add('a001-006nonefull');
+        bloque6.classList.add('a001-006nonefull');
+        bloque7.classList.add('a001-006nonefull');
+        bloque8.classList.add('a001-006nonefull');
+        bloque9.classList.remove('a001-006nonefull');
+        bloque10.classList.add('a001-006nonefull');
+        bloque11.classList.add('a001-006nonefull');
+    })
+    vida10.addEventListener('click', () =>{
+        padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.add('a001-006nonefull');
+        bloque2.classList.add('a001-006nonefull');
+        bloque3.classList.add('a001-006nonefull');
+        bloque4.classList.add('a001-006nonefull');
+        bloque5.classList.add('a001-006nonefull');
+        bloque6.classList.add('a001-006nonefull');
+        bloque7.classList.add('a001-006nonefull');
+        bloque8.classList.add('a001-006nonefull');
+        bloque9.classList.add('a001-006nonefull');
+        bloque10.classList.remove('a001-006nonefull');
+        bloque11.classList.add('a001-006nonefull');
+    })
+    vida11.addEventListener('click', () =>{
+        padre.classList.remove('Spadreseccion-006-none');
+        bloque1.classList.add('a001-006nonefull');
+        bloque2.classList.add('a001-006nonefull');
+        bloque3.classList.add('a001-006nonefull');
+        bloque4.classList.add('a001-006nonefull');
+        bloque5.classList.add('a001-006nonefull');
+        bloque6.classList.add('a001-006nonefull');
+        bloque7.classList.add('a001-006nonefull');
+        bloque8.classList.add('a001-006nonefull');
+        bloque9.classList.add('a001-006nonefull');
+        bloque10.classList.add('a001-006nonefull');
+        bloque11.classList.remove('a001-006nonefull');
     })
 
-    cerrar.addEventListener('click', () =>{
+    cerrar1.addEventListener('click', () =>{
+        padre.classList.add('Spadreseccion-006-none');
+    })
+    cerrar2.addEventListener('click', () =>{
+        padre.classList.add('Spadreseccion-006-none');
+    })
+    cerrar3.addEventListener('click', () =>{
+        padre.classList.add('Spadreseccion-006-none');
+    })
+    cerrar4.addEventListener('click', () =>{
+        padre.classList.add('Spadreseccion-006-none');
+    })
+    cerrar5.addEventListener('click', () =>{
+        padre.classList.add('Spadreseccion-006-none');
+    })
+    cerrar6.addEventListener('click', () =>{
+        padre.classList.add('Spadreseccion-006-none');
+    })
+    cerrar7.addEventListener('click', () =>{
+        padre.classList.add('Spadreseccion-006-none');
+    })
+    cerrar8.addEventListener('click', () =>{
+        padre.classList.add('Spadreseccion-006-none');
+    })
+    cerrar9.addEventListener('click', () =>{
+        padre.classList.add('Spadreseccion-006-none');
+    })
+    cerrar10.addEventListener('click', () =>{
+        padre.classList.add('Spadreseccion-006-none');
+    })
+    cerrar11.addEventListener('click', () =>{
         padre.classList.add('Spadreseccion-006-none');
     })
 
